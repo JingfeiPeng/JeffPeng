bc48a4dc1e74955728764ee422a5750c552a81b0
Jeff Peng <Jeff.Peng@capco.com>
2019-11-17 02:53:00 -0500

    fix build

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/JeffWeb'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/JeffWeb/index.html'));
 });
+
+// app.get('*', (req, res) => {
+//     res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
