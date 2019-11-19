77324443d1effaf9a6298d9a05ffcaeb74d0bf14
Jeff Peng <Jeff.Peng@capco.com>
2019-11-18 22:36:27 -0500

    page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { ProfileComponent } from './mainpage/profile/profile.component';
+import { AboutMeComponent } from './mainpage/about-me/about-me.component';
+import { ProjectsComponent } from './mainpage/projects/projects.component';
+import { ContactInfoComponent } from './mainpage/contact-info/contact-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainpageComponent
+    MainpageComponent,
+    ProfileComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    ContactInfoComponent
   ],
   imports: [
     BrowserModule,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  steps = [
+    {
+      id:"Home",
+      label:"Home"
+    },
+    {
+      id:"AboutMe",
+      label:"About Me"
+    },
+    {
+      id:"Projects",
+      label:"Projects"
+    },
+    {
+      id:"Contact",
+      label:"Contact"
+    }
+  ]
+  @HostListener('window:scroll', ['$event'])
+
+
+  // called when scrolling
+  onScroll(ev: any) {
+    this.scrollManager();
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  scrollManager(){
+    const scrollPosY = window.pageYOffset || document.body.scrollTop;
+    const navContainer = document.getElementById('header');
+    const filler = document.getElementById('filler');
+    if (scrollPosY <= 280) {
+      navContainer.classList.remove('shrinked');
+      filler.classList.remove('fillerHeight');
+    } else {
+      navContainer.classList.add('shrinked');
+      filler.classList.add('fillerHeight');
+
+    }
+
+    const steps = this.steps; // set up steps
+    for (const i in steps) {
+      if (steps.hasOwnProperty(i)) {
+        // get actual target id as html
+        const step = document.getElementById(`${steps[i].id}`) as HTMLElement;
+        const stepYPosition = step.getBoundingClientRect().top;
+        if (stepYPosition < 200 && stepYPosition > 0) { // within 200 of the height of page
+          const stepNav = document.getElementById(
+            `NAV-${steps[i].id}`
+          ) as HTMLElement;
+          // console.log(stepNav)
+          if (stepNav) {
+            const stepNavItems = document.querySelectorAll('.nav-link');
+            [].forEach.call(stepNavItems, (item) => {
+              item.classList.remove('active');
+            });
+            stepNav.classList.add('active');
+          }
+          break;
+        }
+      } 
+    }
+
+  }
+
+  goToStep(stepId : string){
+    let index;
+    for (const i in this.steps) {
+      if (this.steps.hasOwnProperty(i)) {
+        if (stepId === this.steps[i].id) {
+          index = parseInt(i, 10);
+          break;
+        }
+      }
+    }
+    let stepTop = document.getElementById(stepId);
+    if (index === 0){
+      stepTop = document.querySelector('app-header');
+    }
+    stepTop.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
   }
 
 }
