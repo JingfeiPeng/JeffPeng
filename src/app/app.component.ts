d3988b95e9bbb7b83a92fe639e0133afc707ba3a
Jeff Peng <Jeff.Peng@capco.com>
2019-11-30 20:25:22 -0500

    Only show splash screen on first visit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,13 @@ import { SplashScreenTimerService } from './services/splash-screen-timer.service
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  loadingPage : boolean = true;
+  loadingPage : boolean;
   constructor(private timer: SplashScreenTimerService){
+    this.loadingPage = true;
+    let visited = localStorage.getItem("visited");
+    // don't show the splash screen if already visited before
+    if (visited) this.loadingPage = false;
+    else localStorage.setItem("visited","true")
   }
 
   ngOnInit(){
