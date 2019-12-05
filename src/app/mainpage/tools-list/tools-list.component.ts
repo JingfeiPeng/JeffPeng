6dab64211d01f9b83bb839a398bcfdc0f9af5c98
Jeff Peng <Jeff.Peng@capco.com>
2019-12-04 23:50:26 -0500

    added new tools section

---
+++ src/app/mainpage/tools-list/tools-list.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+enum TechStacks {
+  frontEnd = "FRONT_END",
+  backEnd = "BACK_END",
+  others = "OTHERS"
+}
+
+@Component({
+  selector: 'app-tools-list',
+  templateUrl: './tools-list.component.html',
+  styleUrls: ['./tools-list.component.scss']
+})
+export class ToolsListComponent implements OnInit {
+  TechStacks = TechStacks;
+  techOrders = [TechStacks.frontEnd, TechStacks.backEnd, TechStacks.others]
+  techs = {
+    [TechStacks.frontEnd]: ["React", "React Native", "Redux",  "Angular", "Rxjx", "NgRx", "Electron", "Javascript/Typescript",
+    "HTML", "CSS", "SCSS", "Gulp",  "jQuery", "Bootstrap", "Angular Material", "PrimeReact", "PrimeNg"],
+    [TechStacks.backEnd]: ["Node", "Express", "Socket.io","Java", "Spring Boot", "MongoDB", "Mongoose", "SQL"],
+    [TechStacks.others] :["C", "C++", "Python", "PyGame", "Tensorflow", "Numpy", "docker", "Git", "SVN", "Linux", "Bash", "Heroku"]
+  }
+  activeTech : TechStacks = TechStacks.frontEnd;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  highlight(stack: TechStacks){
+    this.activeTech = stack;
+  }
+
+  styleType(stack: TechStacks){
+    if (stack !== this.activeTech){
+      return {
+        "color": "rgb(168, 165, 165)",
+        "transition": "0.5s ease"
+      }
+    } else {
+      return {
+        "color": "black",
+        "transition": "0.5s ease"
+      }
+    }
+  }
+
+}
