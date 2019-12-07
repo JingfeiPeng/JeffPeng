84c4ac44905d7e237178a8a0bc3c9b453a60006f
Jeff Peng <Jeff.Peng@capco.com>
2019-12-07 01:07:05 -0500

    back to master

--- src/app/mainpage/projects/projects.component.ts
+++ src/app/mainpage/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ProjectInfo} from '../../models'
+import { projectData } from './projectData';
 
 @Component({
   selector: 'app-projects',
@@ -7,68 +8,7 @@ import {ProjectInfo} from '../../models'
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-  projects : ProjectInfo[] = [
-    {
-      title:"Two Players Tetris",
-      techs:"C++ & Xwindow Graphic",
-      description:"A two players tetris game project made using C++. Incorporated observer, factory and MVC design patterns. It also used OOP techniques such as Polymorphism, RAII and single responsibility principle",
-      githubLink:"https://github.com/JingfeiPeng/Biquadris",
-      pictureLink:"tetris.jpg",
-      techsUsedList:["C++","Xwindow Graphic"],
-      picturesList:[],
-      additionalInfo:[]
-    },
-    {
-      title:"Energy Station",
-      techs:"React Native & Redux",
-      description:"A mobile app build in React Native, featuring user authentication, manipulating to-do list activities, and manage user’s friend list. Consisting of 10+ screens and 20+ react components",
-      githubLink:"https://github.com/JingfeiPeng/EnergyStation",
-      pictureLink:"energyStation.png",
-      techsUsedList:["React Native", "Redux"],
-      picturesList:[],
-      additionalInfo:[]
-    },
-    {
-      title:"Energy Station Backend",
-      techs:"Node & Express & MongoDB",
-      description:"Created backend server using Node, Express, Mongoose to handle 10+ APIs used in the app. Implemented middleware functions to log errors to text file, authenticate users, and handle exceptions",
-      githubLink:"https://github.com/JingfeiPeng/EnergyStationBackend",
-      pictureLink:"mongodb.jpg",
-      techsUsedList:["Node", "Express", "MongoDb", "Mongoose"],
-      picturesList:[],
-      additionalInfo:[]
-    },
-    {
-      title:"Conqueror",
-      techs: "Python & Pygame",
-      description:"A 2D MMO game where player can play 4 levels, shoot and kill enemies to get gold and upgrade stats, and save game progress.",
-      githubLink:"https://github.com/JingfeiPeng/Conquerer-2D-game",
-      pictureLink:"conqueror.jpg",
-      techsUsedList:["Python","Pygame"],
-      picturesList:[],
-      additionalInfo:[]
-    },
-    {
-      title:"This site",
-      techs:"Angular & gulp & Heroku Hosting",
-      description:"This is the codebase for the site you are seeing now. Available in both English and French",
-      githubLink:"https://github.com/JingfeiPeng/JeffPeng",
-      pictureLink:"JeffSite.png",
-      techsUsedList:["Angular", "gulp", "Heroku Hosting", "NgRx", "RxJs"],
-      picturesList:[],
-      additionalInfo:[]
-    },
-    {
-      title:"Chat App",
-      techs:"React & Node & Express & Socket.io",
-      description:"a real-time chat applications built using react/react hooks. backend uses Node, Express, and Socket.io",
-      githubLink:"https://github.com/JingfeiPeng/JingfeiOnlineChatApp",
-      pictureLink:"chatApp.png",
-      techsUsedList:["React", "Node","Express", "Socket.io"],
-      picturesList:[],
-      additionalInfo:[]
-    }
-  ]
+  projects : ProjectInfo[] = projectData;
   displayProjectDetail: boolean = false;
   projectDetail = null
 
--- src/app/mainpage/projects/specific-detail/specific-detail.component.ts
+++ src/app/mainpage/projects/specific-detail/specific-detail.component.ts
@@ -1,24 +1,43 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation, OnChanges } from '@angular/core';
 import { ProjectInfo } from 'src/app/models';
 
 @Component({
   selector: 'app-specific-detail',
   templateUrl: './specific-detail.component.html',
-  styleUrls: ['./specific-detail.component.scss']
+  styleUrls: ['./specific-detail.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class SpecificDetailComponent implements OnInit {
+export class SpecificDetailComponent implements OnInit, OnChanges {
 
   @Input("displayProjectDetail") display: boolean;
   @Output("closeDetails") closeDetails = new EventEmitter<boolean>()
   @Input("projectDetail") projectDetail : ProjectInfo = <ProjectInfo>{}; 
+  images: any[] = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(){
+    if (this.projectDetail && this.display){
+      let newImages = []
+      for (let picture of this.projectDetail.picturesList){
+        let newImage = {
+          source:`../../../assets/images/${picture}`,
+          alt:'Live screenshots',
+          title: ""
+        }
+        newImages.push(newImage)
+      }
+      this.images = newImages;
+
+    }
+  }
+
   toggleDialog() {
     this.closeDetails.emit(false);
+    // this.images = [];
   }
 
 }
