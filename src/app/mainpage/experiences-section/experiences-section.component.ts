058c8061ae3faa20492096a74f014c60ffcedbea
Jeff Peng <Jeff.Peng@capco.com>
2019-12-20 00:10:48 -0500

    Added template to put projects

---
+++ src/app/mainpage/experiences-section/experiences-section.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+// modified from : https://codepen.io/adventuresinmissions/pen/sJemI
+
+@Component({
+  selector: 'app-experiences-section',
+  templateUrl: './experiences-section.component.html',
+  styleUrls: ['./experiences-section.component.scss']
+})
+export class ExperiencesSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
