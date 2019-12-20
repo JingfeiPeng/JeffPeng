c076cd365fc1364a2be0a3603ef6c0b0457b8956
Jeff Peng <Jeff.Peng@capco.com>
2019-12-20 16:02:53 -0500

    modified to display dynamically

---
+++ src/app/mainpage/experiences-section/experiencesData.ts
@@ -0,0 +1,18 @@
+import { Experience } from "../../models/experience.model";
+
+export const experiencesData: Experience[] = [
+    {
+        title:'CAPCO',
+        descriptions:["DESCRIPTION_1","DESCRIPTION_2","DESCRIPTION_3"],
+        techsUsed:['Angular', 'rxjs', 'NgRx', 'React', 'React Hooks & Contexts', 'Joint.js', 'Node/Express', 'Electron', 'MongoDB', 'docker'],
+        period:"Sept 2019 - Dec 2019",
+        logoImg:"capco.jpeg"
+    },
+    {
+        title:'MMC',
+        descriptions:["DESCRIPTION_1","DESCRIPTION_2","DESCRIPTION_3"],
+        techsUsed:["React Native", "Redux", "UML", "SQL"],
+        period:"Jan 2019 - Apr 2019",
+        logoImg:"mmc.png"
+    }
+]
---
+++ src/app/models/experience.model.ts
@@ -0,0 +1,7 @@
+export interface Experience {
+    title: string,
+    descriptions: string[],
+    techsUsed:string[],
+    period:string,
+    logoImg:string
+}
