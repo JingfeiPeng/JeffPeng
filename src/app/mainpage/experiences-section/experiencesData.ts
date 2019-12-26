a5b11455fce5f669f78137c22f4aca10a70a1524
JingfeiPeng <pengjefferbms@gmail.com>
2019-12-26 18:28:27 -0500

    Added experiences data

--- src/app/mainpage/experiences-section/experiencesData.ts
+++ src/app/mainpage/experiences-section/experiencesData.ts
@@ -3,7 +3,13 @@ import { Experience } from "../../models/experience.model";
 export const experiencesData: Experience[] = [
     {
         title:'CAPCO',
-        descriptions:["DESCRIPTION_1","DESCRIPTION_2","DESCRIPTION_3"],
+        descriptions:[{
+            title:"DESCRIPTION_1", details:["D1_D1","D1_D2"]
+        },{
+            title:"DESCRIPTION_2", details:[]
+        },{
+            title:"DESCRIPTION_3", details:[]
+        }],
         techsUsed:['Angular', 'rxjs', 'NgRx', 'React', 'React Hooks & Contexts', 'Joint.js', 'Node/Express', 'Electron', 'MongoDB', 'docker'],
         period:"Sept 2019 - Dec 2019",
         logoImg:"capco.jpeg",
@@ -11,7 +17,13 @@ export const experiencesData: Experience[] = [
     },
     {
         title:'MMC',
-        descriptions:["DESCRIPTION_1","DESCRIPTION_2","DESCRIPTION_3"],
+        descriptions:[{
+            title:"DESCRIPTION_1", details:[]
+        },{
+            title:"DESCRIPTION_2", details:[]
+        },{
+            title:"DESCRIPTION_3", details:[]
+        }],
         techsUsed:["React Native", "Redux", "UML", "SQL"],
         period:"Jan 2019 - Apr 2019",
         logoImg:"mmc.png",
@@ -19,7 +31,13 @@ export const experiencesData: Experience[] = [
     },
     {
         title:"Perfect Financial Solutions",
-        descriptions:["DESCRIPTION_1","DESCRIPTION_2","DESCRIPTION_3"],
+        descriptions:[{
+            title:"DESCRIPTION_1", details:[]
+        },{
+            title:"DESCRIPTION_2", details:[]
+        },{
+            title:"DESCRIPTION_3", details:[]
+        }],
         techsUsed:["HTML", "CSS", "JavaScript", "jQuery", "Bootstrap", "PHP", "SQL"],
         period:"May 2018 - Aug 2018",
         logoImg:"",
--- src/app/models/experience.model.ts
+++ src/app/models/experience.model.ts
@@ -1,8 +1,13 @@
 export interface Experience {
     title: string,
-    descriptions: string[],
+    descriptions: Description[],
     techsUsed:string[],
     period:string,
     logoImg:string,
     show: boolean
+}
+
+export interface Description{
+    title: string,
+    details: string[]
 }
